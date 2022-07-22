9f923782ae6a05152134337e5117b73eb5494015
bolshu <bolshu@Vadim-Akulin-MacBook-Pro.local>
2022-07-22 23:11:40 +0300

    generate invaders

--- src/modules/Invaders.ts
+++ src/modules/Invaders.ts
@@ -66,16 +66,16 @@ class Invader {
 export class Invaders implements IDrawable {
   private readonly INVADER_SHAPES: TInvaderShape[] = ['circle', 'square', 'cross', 'plus']
   private readonly GAP: number = 10
-  private readonly SIZE: number = 40
-  private readonly ROWS_MULTIPLIER: number = 0.25
-  private readonly COLS_MULTIPLIER: number = 0.7
+  private readonly INVADER_SIZE: number = 40
+  private readonly ROWS_MULTIPLIER: number = 0.3
+  private readonly COLS_MULTIPLIER: number = 0.6
   private readonly VELOCITY_X: number = 0.5
-  private readonly VELOCITY_Y: number = this.SIZE / 2
+  private readonly VELOCITY_Y: number = this.INVADER_SIZE / 2
   private position: TCoordinates
   private velocity: TCoordinates
   private cols: number
   private rows: number
-  private inviders: Invader[] = []
+  public inviders: Invader[] = []
 
   constructor (canvas: HTMLCanvasElement) {
     this.position = {
@@ -86,8 +86,8 @@ export class Invaders implements IDrawable {
       x: this.VELOCITY_X,
       y: this.VELOCITY_Y
     }
-    this.cols = Math.floor(canvas.width * this.COLS_MULTIPLIER / this.SIZE)
-    this.rows = Math.floor(canvas.width * this.ROWS_MULTIPLIER / this.SIZE)
+    this.cols = Math.floor(canvas.width * this.COLS_MULTIPLIER / this.INVADER_SIZE)
+    this.rows = Math.floor(canvas.height * this.ROWS_MULTIPLIER / this.INVADER_SIZE)
 
     this.createGroup()
   }
@@ -96,12 +96,12 @@ export class Invaders implements IDrawable {
     for (let indexCol = 0; indexCol < this.cols; indexCol++) {
       for (let indexRow = 0; indexRow < this.rows; indexRow++) {
         const position: TCoordinates = {
-          x: this.position.x + indexCol * this.SIZE + this.GAP * indexCol,
-          y: this.position.y + indexRow * this.SIZE + this.GAP * indexRow
+          x: this.position.x + indexCol * this.INVADER_SIZE + this.GAP * indexCol,
+          y: this.position.y + indexRow * this.INVADER_SIZE + this.GAP * indexRow
         }
         const shape = this.INVADER_SHAPES[Math.floor(Math.random() * this.INVADER_SHAPES.length)]
 
-        this.inviders.push(new Invader(position, this.SIZE, shape))
+        this.inviders.push(new Invader(position, this.INVADER_SIZE, shape))
       }
     }
   }
@@ -115,7 +115,7 @@ export class Invaders implements IDrawable {
   public update (canvas: HTMLCanvasElement): void {
     this.velocity.y = 0
 
-    if (this.position.x + this.SIZE * this.cols + this.GAP * this.cols > canvas.width || this.position.x < 0) {
+    if (this.position.x + this.INVADER_SIZE * this.cols + this.GAP * this.cols > canvas.width || this.position.x < 0) {
       this.velocity.x = -this.velocity.x
       this.velocity.y = this.VELOCITY_Y
     }
